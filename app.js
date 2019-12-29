7eaa96ee6dc274624409e8a30a828824a795646e
George Schafer <gee.ess.mbo@gmail.com>
2019-12-28 19:16:58 -0700

    finishing touches

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ function getWeather(){
 
   weather.getWeather()
        .then(results => {
+        // console.log(results);
         ui.paint(results);
-        console.log(results);
        })
        .catch(err => console.log(err));
 
--- ui.js
+++ ui.js
@@ -12,14 +12,15 @@ class UI {
   }
 
   paint(weather){
-    let dewpoint = (weather.main.temp - ((9/25)*(100-weather.main.humidity))).toFixed(2) ;
+    // Dewpoint calculation can be found at https://en.wikipedia.org/wiki/Dew_point
+    let dewpoint = (weather.main.temp - ((9/25)*(100-weather.main.humidity))).toFixed(1) ;
     this.location.textContent = weather.name;
     this.desc.textContent = weather.weather[0].description;
     this.string.textContent = `${weather.main.temp} F`;
     // this.icon.setAttribute('src', weather.xxxxx);
     this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`;
     this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like} F`;
-    this.dewpoint.textContent = `DewPoint: ${dewpoint} F`;
+    this.dewpoint.textContent = `Dewpoint: ${dewpoint} F`;
     this.wind.textContent = `Wind: ${weather.wind.speed} m/hr`; 
   }
 
